d0c92790385ef78c35c223c24851f326b39d2246
IssaL <aa10240tw@gmail.com>
2015-12-26 00:49:57 +0800

    support>0.5

--- mining.R
+++ mining.R
@@ -3,4 +3,8 @@ library(arules);##association library
 library(plyr);#clean data
 ##read data
 origin <- read.csv("carprefs.csv",header = TRUE);
-##data cleaning
+#data need to be factor or logic, and origin must be type data.frame
+origin <- as.data.frame(apply(origin,MARGIN = 2,FUN = as.factor));
+##association
+rules <- apriori(origin,parameter = list(support=0.4,conf=0),control = list());
+inspect(rules);
